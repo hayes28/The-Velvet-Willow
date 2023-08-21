18eac9e260199ea7d33e708009e2f5c2e28626c0
HH <heather.hayes918@gmail.com>
2023-08-20 21:33:24 -0500

    Contact form working

--- velvet-willow/src/config/firebase.js
+++ velvet-willow/src/config/firebase.js
@@ -1,22 +1,46 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA1Q-03aFHaqi7NHINyvCi59huh58_e9JE",
-  authDomain: "the-velvet-willow.firebaseapp.com",
-  databaseURL: "https://the-velvet-willow-default-rtdb.firebaseio.com",
-  projectId: "the-velvet-willow",
-  storageBucket: "the-velvet-willow.appspot.com",
-  messagingSenderId: "167195773265",
-  appId: "1:167195773265:web:675d989e828b0001bb2b70",
-  measurementId: "G-833EM2M0ZP"
+  apiKey: "AIzaSyABqHximBaL6M1sBnHENryip1QyC6qgaV4",
+  authDomain: "velvet-willow.firebaseapp.com",
+  databaseURL: "https://velvet-willow-default-rtdb.firebaseio.com/",
+  projectId: "velvet-willow",
+  storageBucket: "velvet-willow.appspot.com",
+  messagingSenderId: "256830007264",
+  appId: "1:256830007264:web:2b7af5b9f6cb6f1474ed85",
+  measurementId: "G-74GN5QMSZW"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+
+// Refernece contactInfo collections
+let contactInfo = firebase.database().ref("infos");
+
+
+// Listen for a submit
+document.querySelector(".contact-form").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  //   Get input Values
+  let name = document.querySelector(".name").value;
+  let email = document.querySelector(".email").value;
+  let message = document.querySelector(".message").value;
+  console.log(name, email, message);
+
+  saveContactInfo(name, email, message);
+
+  document.querySelector(".contact-form").reset();
+}
+
+// Save infos to Firebase
+function saveContactInfo(name, email, message) {
+  let newContactInfo = contactInfo.push();
+
+  newContactInfo.set({
+    name: name,
+    email: email,
+    message: message,
+  });
+}
